244db1c55e50982d566cb65d832d771520bf8fc4
kkosuke <kkosuke007@gmail.com>
2023-01-04 23:00:54 +0900

    initと仮Postデータ作成

---
+++ README.md
@@ -0,0 +1,40 @@
+# 環境
+- ruby 3.0.0
+- node 18.7.0
+
+# DB環境
+- docker-compose.yml
+
+## 入り方
+```
+$ docker ps
+CONTAINER ID   IMAGE
+XXXX           mysql:8.0.20
+
+$ docker exec -it XXXX mysql -u root -p
+Enter password:
+# docker-compose.ymlの、DB_PASSWORD の値を入力
+```
+
+# 起動方法
+## 1
+```
+$ ngrok http 8080
+```
+その値を以下のfileに記載する。
+
+- project-backend/.env
+- project-frontend/.env.local
+
+※ ngrok をリスタートするたびに変えてください。
+
+## 2
+```
+$ docker-compose up
+```
+で、それぞれの Dockerfile の CMD のコードが叩かれる。
+
+その後、以下のURLで、NEXT.JSや、Rails API、Sidekiqの管理画面が表示されることを確認できる。
+- http://localhost:3000/
+- http://localhost:8080/
+- http://localhost:8080/sidekiq
---
+++ project-frontend/pages/index.js
@@ -0,0 +1,36 @@
+import Error from 'next/error';
+
+const Home = ({ posts, statusCode }) => {
+  if (statusCode) {
+    return <Error statusCode={statusCode} />;
+  }
+
+  return (
+    <div>
+      <h1>POSTS</h1>
+      <ul>
+        {!posts
+          ? null
+          : posts.map((post, index) => {
+              return <li key={index}>{post.title}</li>;
+            })}
+      </ul>
+    </div>
+  );
+};
+
+Home.getInitialProps = async (_ctx) => {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/posts`);
+    const data = await res.json();
+    return {
+      posts: data.posts,
+    };
+  } catch (error) {
+    return {
+      statusCode: error.response ? error.response.status : 500,
+    };
+  }
+};
+
+export default Home;
